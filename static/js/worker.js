e3088fe4019384434c4799eeb6ca969f5d0cb333
AlexisJr2004 <duranalexis879@gemail.com>
2024-10-02 08:04:46 -0500

    ChatBot GPT

--- static/js/worker.js
+++ static/js/worker.js
@@ -1,17 +1,17 @@
-import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
-
-const handler = new WebWorkerMLCEngineHandler()
-
-self.onmessage = (msg) => {
-  handler.onmessage(msg)
-}
-
-
-
-// // import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm" // Descomentar para usar el modelo de 5.1 GB
+// import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
 
 // const handler = new WebWorkerMLCEngineHandler()
 
 // self.onmessage = (msg) => {
 //   handler.onmessage(msg)
 // }
+
+
+
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm" // Descomentar para usar el modelo de 5.1 GB
+
+const handler = new WebWorkerMLCEngineHandler()
+
+self.onmessage = (msg) => {
+  handler.onmessage(msg)
+}
